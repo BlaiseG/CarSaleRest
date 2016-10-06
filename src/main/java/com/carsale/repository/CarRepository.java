cec6cc9ba2e3e9e10ee00d6fca5f8e356fd7af24
Balazs Gerendai <balazs_gerendai@epam.com>
2016-10-06 12:01:04 +0200

    JPA repository changed to SortableRepository to fix missing search endpoint

--- src/main/java/com/carsale/repository/CarRepository.java
+++ src/main/java/com/carsale/repository/CarRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
@@ -13,7 +14,7 @@ import com.carsale.model.Car;
 
 @Repository
 @RepositoryRestResource(path="cars")
-public interface CarRepository extends JpaRepository<Car, Long> {
+public interface CarRepository extends PagingAndSortingRepository<Car, Long> {
 	Page<Car> findByModelName(@Param("modelName") String modelName, Pageable pageable);
 	List<Car> findAllCarsByModelName(@Param("modelName") String modelName);
 	
